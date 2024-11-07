9c5dac76a072db725af9ffdc48a56b68362f7f62
Vladislav <senior.vlados97@gmail.com>
2024-11-07 22:09:56 +0300

    Implement GasStove class and its methods

--- src/main/java/ru/bstu/itz212/fokin/lab3/GasStove.java
+++ src/main/java/ru/bstu/itz212/fokin/lab3/GasStove.java
@@ -8,8 +8,15 @@
 
 package ru.bstu.itz212.fokin.lab3;
 
+import ru.bstu.itz212.fokin.lab3.enums.GasStoveType;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 
 /**
@@ -19,7 +26,7 @@ public class GasStove extends Appliance {
     private String name;
     private String manufacturer;
     private String serialNumber;
-    private String type;
+    private GasStoveType type;
     private Date productionDate;
     private double price;
     private byte burnerCount;
@@ -39,16 +46,73 @@ public class GasStove extends Appliance {
         System.out.println("Введите серийный номер: ");
         serialNumber = scanner.nextLine();
 
-        System.out.println("Введите тип плиты: ");
+        System.out.println("""
+                Введите тип плиты:
+                1. отдельно стоящая;
+                2. встраиваемая;
+                3. портативная.
+                """);
+
+        int input = scanner.nextInt();
+        type = switch (input) {
+            case 1 -> GasStoveType.FREESTANDING;
+            case 2 -> GasStoveType.BUILT_IN;
+            case 3 -> GasStoveType.PORTABLE;
+            default -> GasStoveType.FREESTANDING;
+        };
+
+        System.out.println("Введите дату производства (дд.мм.гггг): ");
+        String dateStr = scanner.nextLine();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
+
+        boolean inputError;
+        do {
+            try {
+                productionDate = dateFormatter.parse(dateStr);
+                inputError = false;
+            } catch (ParseException e) {
+                System.out.println("Некорректный ввод даты. Дата должна быть в формате дд.мм.гггг");
+                inputError = true;
+            }
+        } while (inputError);
+
+
+        System.out.println("Введите количество конфорок: ");
+        burnerCount = scanner.nextByte();
+
+        System.out.println("Наличие электронного поджога (да/нет): ");
+        electricIgnition = parseYesOrNo(scanner.nextLine());
+
+        if (!type.equals(GasStoveType.PORTABLE)) {
+            System.out.println("Наличие духовки (да/нет): ");
+            oven = parseYesOrNo(scanner.nextLine());
+
+            if (oven) {
+                System.out.println("Наличие гриля в духовке (да/нет): ");
+                grill = parseYesOrNo(scanner.nextLine());
+            }
+        }
+
+        System.out.println("Наличие таймера (да/нет): ");
+        timer = parseYesOrNo(scanner.nextLine());
+
+        System.out.println("Введите стоимость: ");
+        price = scanner.nextDouble();
     }
 
     @Override
     public String getCost() {
-        return "";
+        return String.format("%f.3 руб.", price);
     }
 
     @Override
     public String toString() {
         return "";
+    }
+
+    private boolean parseYesOrNo(String input) {
+        input = input.toLowerCase();
+
+        return input.equals("да");
     }
 }
--- src/main/java/ru/bstu/itz212/fokin/lab3/Main.java
+++ src/main/java/ru/bstu/itz212/fokin/lab3/Main.java
@@ -3,6 +3,7 @@ package ru.bstu.itz212.fokin.lab3;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
